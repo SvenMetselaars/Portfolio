508e77007ebbeb60dcf499c12724728d6980422d
Sven Metselaars <91815@roc-teraa.nl>
2025-10-21 19:09:41 +0200

    deleted test console log
    i deleted the test console log in the js file

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function changeScreen(newscreen) {
   const screens = document.querySelectorAll('.screen');
 
   screens.forEach((el, index) => {
-    console.log("test")
     el.classList.toggle('shown', index + 1 === newscreen);
     el.classList.toggle('hidden', index + 1 !== newscreen);
   });
